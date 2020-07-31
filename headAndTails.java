06fb0b830fee7b02425dc7eea9c63405522d5078
WILL <ryo08310831@icloud.com>
2020-07-31 11:43:49 +0900

    HOT: Initial revision

---
+++ headAndTails.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+class headAndTails {
+
+  public static void main(String[] args) {
+    int head=0;
+    int tail=0;
+
+    System.out.println("Tossing a coin...");
+    for(int i=1;i<4;i++){
+      Random r = new Random();
+      int rand = r.nextInt(2);
+      if(rand == 0){
+        System.out.println("Round " + i +": Heads");
+        head++;
+      }else{
+        System.out.println("Round " + i +": Tails");
+        tail++;
+      }
+    }
+    System.out.println("Heads: " + head + ", Tails: " + tail);
+  }
+}

3ecc1b4a1d78baa1feae35b7edec8c03ce041e3e
WILL <ryo08310831@icloud.com>
2020-07-31 12:04:12 +0900

    Final revision

--- headAndTails.java
+++ headAndTails.java
@@ -28,9 +28,9 @@ class headAndTails {
     System.out.println("Heads: " + head + ", Tails: " + tail);
 
     if(head > tail){
-      System.out.println("You won");
+      System.out.println(name + " won");
     }else{
-      System.out.println("You lost");
+      System.out.println(name + " lost");
     }
   }
 }
